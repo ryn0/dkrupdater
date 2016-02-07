16764488833156ac1f5410a9641f1974667dbbe0
ryn0 <admin@bootbaron.com>
2016-02-07 11:06:52 -0800

    name folder 'playlist'

--- DKRUpdater.Core/Constants/StringConstants.cs
+++ DKRUpdater.Core/Constants/StringConstants.cs
@@ -5,7 +5,7 @@ namespace DKRUpdater.Core.Constants
         public const string Mp3DownloadDirectory = @"C:\MP3s\downloads";
         public const string Mp3LogsDirectory = @"C:\MP3s\logs";
         public const string Mp3MusicDirectory = @"C:\MP3s\music";
-        public const string Mp3PlaylistsDirectory = @"C:\MP3s\playlists";
+        public const string Mp3PlaylistsDirectory = @"C:\MP3s\playlist";
         public const string NewMusicPlaylist = @"C:\MP3s\playlist\newmusic.pls";
     }
 }
