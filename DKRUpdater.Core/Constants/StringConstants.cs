0c0032536d54f4dffa642c19730c7a1cb0fa8add
ryn0 <admin@bootbaron.com>
2016-02-12 03:02:14 -0800

    comments

--- DKRUpdater.Core/Constants/StringConstants.cs
+++ DKRUpdater.Core/Constants/StringConstants.cs
@@ -2,10 +2,13 @@ namespace DKRUpdater.Core.Constants
 {
     public class StringConstants
     {
+        // todo: refactor out all paths
         public const string Mp3DownloadDirectory = @"C:\MP3s\downloads";
         public const string Mp3LogsDirectory = @"C:\MP3s\logs";
         public const string Mp3MusicDirectory = @"C:\MP3s\music";
         public const string Mp3PlaylistsDirectory = @"C:\MP3s\playlist";
+
+        // todo: refactor to just the newmusic.pls
         public const string NewMusicPlaylist = @"C:\MP3s\playlist\newmusic.pls";
     }
 }
